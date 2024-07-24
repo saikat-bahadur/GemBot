6c5382ed08b471c7cdb95285f0efb768e6c8b9d4
Saikat Bahadur <saikatbahadur1234@gmail.com>
2024-07-24 22:35:51 +0530

    2nd commit

--- app/src/main/java/com/example/gembot/ChatPage.kt
+++ app/src/main/java/com/example/gembot/ChatPage.kt
@@ -3,14 +3,24 @@ package com.example.gembot
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -20,10 +30,43 @@ fun ChatPage(modifier: Modifier =Modifier){
         modifier=modifier
     ){
         AppHeader()
+        MessageInput()
 
     }
 
 }
+
+@Composable
+fun MessageInput(){
+    var message by remember{
+        mutableStateOf("")
+    }
+
+    Row(
+        modifier=Modifier.padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        OutlinedTextField(
+            modifier=Modifier.weight(1f),
+            value = message,
+            onValueChange ={
+                message=it
+            } )
+
+        IconButton(onClick={}){
+            Icon(
+                imageVector= Icons.Default.Send,
+                contentDescription="send"
+            )
+
+        }
+    }
+
+}
+
+
+
+
 
 @Composable
 fun AppHeader(){
